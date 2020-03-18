13b0259bf603b8332384c84e6a9e1613102b02be
Cesar Morales <cesarlenin2@live.com>
2020-03-18 11:38:16 -0700

    got alt attributer for small img

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
-$('.thumbnail').on('click', e => {
-    console.log(e.target);
-  });
+'use strict';
+//when a user selects one of the thumbnail images using 
+//the mouse or keyboard, that image should be displayed in the
+// full-size image container at the top. 
+
+//$('.thumbnail') selector for img
+
+//$('.hero img') selector for large img
+
+//all and src attribute for small img should be copied to 
+//to large img when clicked 
+
+$('.thumbnail').click(function(event){
+  event.preventDefault();
+  const smallSrcAttr = $(event.currentTarget).find('img').attr('alt');
   
+  console.log(smallSrcAttr);
+})
