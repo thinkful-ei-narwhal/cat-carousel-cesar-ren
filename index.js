72c11cc3eb2b5d28deabfb99b9db80f2ff442bbb
Ren F <renfinn89@gmail.com>
2020-03-18 11:53:29 -0700

    finished

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 'use strict';
-//when a user selects one of the thumbnail images using 
+//when a user selects one of the thumbnail images using
 //the mouse or keyboard, that image should be displayed in the
-// full-size image container at the top. 
+// full-size image container at the top.
 
 //$('.thumbnail') selector for img
 
 //$('.hero img') selector for large img
 
-//all and src attribute for small img should be copied to 
-//to large img when clicked 
+//all and src attribute for small img should be copied to
+//to large img when clicked
 
-$('.thumbnail').click(function(event){
+$('.thumbnail').click(function(event) {
   event.preventDefault();
-  const smallSrcAttr = $(event.currentTarget).find('img').attr('alt');
-  
-  console.log(smallSrcAttr);
-})
+  const smallAlt = $(event.currentTarget)
+    .find('img')
+    .attr('alt');
+
+  //$( "#greatphoto" ).attr( "alt", "Beijing Brush Seller" )
+
+  const smallSrc = $(event.currentTarget)
+    .find('img')
+    .attr('src');
+
+  $('.hero img')
+    .attr('alt', smallAlt)
+    .attr('src', smallSrc);
+
+  console.log(smallSrc);
+});
